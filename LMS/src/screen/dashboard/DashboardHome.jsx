eb0cebb37798eac020b4131c605fd59cb7aa11c8
nooblearner08 <82766940+nooblearner08@users.noreply.github.com>
2024-02-23 15:07:28 +0530

    ft

---
+++ LMS/src/screen/dashboard/DashboardHome.jsx
@@ -0,0 +1,15 @@
+// style
+import "../../style/dashboardHome.scss";
+
+export default function DashboardHome() {
+  return (
+    <section className="dashboardHome">
+      <div className="description">
+        <h1>Welcome to Admin Panel 😀</h1>
+        <p>
+        Your premier destination for connecting with elite developers worldwide, offering a curated platform for collaboration, learning, and showcasing cutting-edge expertise.
+        </p>
+      </div>
+    </section>
+  );
+}
